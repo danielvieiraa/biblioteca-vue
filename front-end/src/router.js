b0b5ddb2e99694fcd058b3837d744bb96a6c1fdd
maiconkl <52840686+maiconkl@users.noreply.github.com>
2024-06-24 20:52:12 -0300

    Add files via upload

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -3,13 +3,25 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Navbar from './componentes/Navbar.vue';
 import Autor from './paginas/Autor.vue';
 import AutorCadastro from './paginas/AutorCadastro.vue';
-import Home from './paginas/Home.vue'
+import Livro from './paginas/Livro.vue';
+import LivroCadastro from './paginas/LivroCadastro.vue';
+import Funcionario from './paginas/Funcionario.vue';
+import FuncionarioCadastro from './paginas/FuncionarioCadastro.vue';
+
+
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', component: Navbar },
   { path: '/Autores', component: Autor }, // Rota para listar os autores
   { path: '/Autor', component: AutorCadastro }, // Rota para adicionar um novo autor
-  { path: '/Autor/:id', component: AutorCadastro } // Rota para editar um autor existente
-];
+  { path: '/Autor/:id', component: AutorCadastro }, // Rota para editar um autor existente
+  {path: '/Livros', component: Livro}, // Rota para listar livros
+  {path: '/Livro', component: LivroCadastro}, //Rota para adicionar novo livro
+  {path: '/Livro/:id', component: LivroCadastro}, // Rota para Altera Livro
+  {path: '/Funcionarios', component: Funcionario}, //Rota para listar funcionarios
+  {path: '/Funcionario', component: FuncionarioCadastro}, //Rota para Inserir Funcionario
+  {path: '/Funcionario/:id', component: FuncionarioCadastro}, //Rota para Alterar Funcionario
+
+]
 
 
 const router = createRouter({
