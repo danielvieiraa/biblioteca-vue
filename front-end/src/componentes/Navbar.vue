b5cdae2a9b03a1427330001ccf3f6ce97efb1ff1
GuilhermeBottcher <134150519+GuilhermeBottcher@users.noreply.github.com>
2024-06-24 19:52:30 -0300

    Add files via upload

--- front-end/src/componentes/Navbar.vue
+++ front-end/src/componentes/Navbar.vue
@@ -4,40 +4,66 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Biblioteca</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/about">Empréstimos</router-link>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Cadastros
-              </a>
-              <ul class="dropdown-menu">
-                <li><router-link class="dropdown-item" to="/people">Pessoas</router-link></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><router-link class="dropdown-item" to="/books">Livros</router-link></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><router-link class="dropdown-item" to="/employees">Funcionários</router-link></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><router-link class="dropdown-item" to="/autores">Autores</router-link></li>
-                <li><router-link class="dropdown-item" to="/categories">Categoria</router-link></li>
-                <li><router-link class="dropdown-item" to="/publishers">Editora</router-link></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Biblioteca</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">Empréstimos</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Cadastros
+            </a>
+            <ul class="dropdown-menu">
+              <li><router-link class="dropdown-item" to="/people">Pessoas</router-link></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><router-link class="dropdown-item" to="/books">Livros</router-link></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><router-link class="dropdown-item" to="/employees">Funcionários</router-link></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><router-link class="dropdown-item" to="/autores">Autores</router-link></li>
+              <li><router-link class="dropdown-item" to="/categories">Categoria</router-link></li>
+              <li><router-link class="dropdown-item" to="/publishers">Editora</router-link></li>
+            </ul>
+          </li>
+        </ul>
       </div>
-    </nav>
-  </template>
+    </div>
+  </nav>
 
+ 
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+.background-image {
+  background-image: url('img.jpg');
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  text-align: center;
+}
+
+.text-content {
+  background-color: rgba(0, 0, 0, 0.5); 
+  padding: 100px;
   
+}
+</style>
--- front-end/src/router.js
+++ front-end/src/router.js
@@ -3,9 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Navbar from './componentes/Navbar.vue';
 import Autor from './paginas/Autor.vue';
 import AutorCadastro from './paginas/AutorCadastro.vue';
-
+import Home from './paginas/Home.vue'
 const routes = [
-  { path: '/', component: Navbar },
+  { path: '/', component: Home },
   { path: '/Autores', component: Autor }, // Rota para listar os autores
   { path: '/Autor', component: AutorCadastro }, // Rota para adicionar um novo autor
   { path: '/Autor/:id', component: AutorCadastro } // Rota para editar um autor existente
